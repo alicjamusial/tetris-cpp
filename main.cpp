da3bd07338610ca0cc8ff84074b8a2e8d9d7ba08
alicjamusial <alicja@musial.pro>
2020-03-08 10:57:25 +0100

    repo init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include "SDL.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
