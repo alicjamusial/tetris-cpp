33f6995ad355d1b2dd46b195d2bd1b4e9f686ab4
alicjamusial <alicja@musial.pro>
2020-03-20 09:41:47 +0100

    Tetris refactor

--- tetris/Tetris.cpp
+++ tetris/Tetris.cpp
@@ -2,59 +2,54 @@
 // Created by Alicja on 2020-03-20.
 //
 #include <cstdio>
+#include <string>
 #include <SDL.h>
 
 #include "Tetris.hpp"
 
 using namespace game;
 
-Tetris::Tetris() : screenHeight(640), screenWidth(480) {
-  bool quit = false;
-  SDL_Window* window = nullptr;
-  SDL_Surface* screenSurface = nullptr;
+Tetris::Tetris() {
 
-  //Initialize SDL
-  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-  {
-    printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-  }
-  else
-  {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    showError("SDL could not initialize!");
+  } else {
     //Create window
-    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN );
-    if( window == NULL )
-    {
-      printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-    }
-    else
-    {
-      //Get window surface
-      screenSurface = SDL_GetWindowSurface( window );
+    window = SDL_CreateWindow(
+        windowTitle.c_str(),
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        screenWidth, screenHeight,
+        SDL_WINDOW_SHOWN);
 
-      //Fill the surface white
-      SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+    if (window == nullptr) {
+      showError("Window could not be created!");
+    } else {
+      screenSurface = SDL_GetWindowSurface(window);
 
-      //Update the surface
-      SDL_UpdateWindowSurface( window );
+      SDL_FillRect(
+          screenSurface,
+          NULL,
+          SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF));
 
-      //Wait two seconds
-      while (!quit) {
+      SDL_UpdateWindowSurface(window);
+
+      while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
           /* handle your event here */
 
-          //User requests quit
-          if( event.type == SDL_QUIT )
-            quit = true;
+          if(event.type == SDL_QUIT)
+            running = false;
         }
-        /* do some other stuff here -- draw your app, etc. */
       }
     }
   }
 
-  //Destroy window
   SDL_DestroyWindow( window );
-
-  //Quit SDL subsystems
   SDL_Quit();
+}
+
+void Tetris::showError(const std::string& errorMsg) {
+  printf("%s SDL_Error: %s\n", errorMsg.c_str(), SDL_GetError());
 }
--- tetris/Tetris.hpp
+++ tetris/Tetris.hpp
@@ -2,15 +2,24 @@
 // Created by Alicja on 2020-03-20.
 //
 #pragma once
+#include <string>
 #include <cstdint>
+#include <SDL.h>
 
 namespace game {
   class Tetris {
   public:
     Tetris();
 
+    static void showError(const std::string& errorMsg);
+
   private:
-    uint16_t const screenWidth;
-    uint16_t const screenHeight;
+    uint16_t const screenWidth {640};
+    uint16_t const screenHeight {480};
+    std::string windowTitle {"Tetris"};
+
+    bool running {true};
+    SDL_Window* window {nullptr};
+    SDL_Surface* screenSurface {nullptr};
   };
 }
