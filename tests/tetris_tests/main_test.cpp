82b69294c20722f347804734f692a36360c488a6
alicjamusial <alicja@musial.pro>
2020-03-23 08:44:06 +0100

    Clang refactor

--- tests/tetris_tests/main_test.cpp
+++ tests/tetris_tests/main_test.cpp
@@ -1,12 +1,13 @@
 #include "gtest/gtest.h"
-
 // TODO: tests
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
 
-TEST(main_test, test_eq) {
-  EXPECT_EQ(1, 1);
+TEST(main_test, test_eq)
+{
+    EXPECT_EQ(1, 1);
 }
