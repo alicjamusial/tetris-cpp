849941e4d2cb3a633477681d11f7f3baed1f0f13
alicjamusial <alicja@musial.pro>
2020-03-25 19:08:56 +0100

    CMake - change naming of project & target and move target includes

--- tests/tetris_tests/main_test.cpp
+++ tests/tetris_tests/main_test.cpp
@@ -1,12 +1,6 @@
 #include "gtest/gtest.h"
 // TODO: tests
 
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
 TEST(main_test, test_eq)
 {
     EXPECT_EQ(1, 1);
